580901a62747a57c33b3548aa9a289d2d4a181c8
Rahul Salecha <rahulgsalecha@gmail.com>
2018-02-28 18:17:26 -0800

    Python program for implementation of Quicksort Sort

---
+++ Python_Dsc/QuickSort.py
@@ -0,0 +1,99 @@
+# Python program for implementation of Quicksort Sort
+
+# This function takes last element as pivot, places
+# the pivot element at its correct position in sorted
+# array, and places all smaller (smaller than pivot)
+# to left of pivot and all greater elements to right
+# of pivot
+
+def partition(arr,low,high):
+    i = (low - 1) #index of smaller element
+    pivot = arr[high] #pivot
+
+    # If current element is smaller than or
+    # equal to pivot
+    for j in range(low,high):
+
+        # increment index of smaller element
+        if(arr[j] <= pivot):
+            i = i+1
+            arr[i],arr[j] = arr[j],arr[i]
+
+    arr[i+1], arr[high] = arr[high],arr[i+1]
+    return (i+1)
+
+
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low  --> Starting index,
+# high  --> Ending index
+ 
+# Function to do Quick sort
+def quickSort(arr,low,high):
+    if low < high:
+
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr,low,high)
+        
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+print ("Input Array is:")
+print arr
+n = len(arr)
+quickSort(arr,0,n-1)
+print ("Quick Sorted array is:")
+print arr
+
+''' Output:
+    Input Array is:
+    [10, 7, 8, 9, 1, 5]
+    Quick Sorted array is:
+    [1, 5, 7, 8, 9, 10]
+'''
+
+'''
+How partition works:
+
+arr[] = {10, 80, 30, 90, 40, 50, 70}
+Indexes:  0   1   2   3   4   5   6 
+
+low = 0, high =  6, pivot = arr[h] = 70
+Initialize index of smaller element, i = -1
+
+Traverse elements from j = low to high-1
+j = 0 : Since arr[j] <= pivot, do i++ and swap(arr[i], arr[j])
+i = 0 
+arr[] = {10, 80, 30, 90, 40, 50, 70} // No change as i and j are same
+
+j = 1 : Since arr[j] > pivot, do nothing
+// No change in i and arr[]
+
+j = 2 : Since arr[j] <= pivot, do i++ and swap(arr[i], arr[j])
+i = 1
+arr[] = {10, 30, 80, 90, 40, 50, 70} // We swap 80 and 30 
+
+j = 3 : Since arr[j] > pivot, do nothing
+// No change in i and arr[]
+
+j = 4 : Since arr[j] <= pivot, do i++ and swap(arr[i], arr[j])
+i = 2
+arr[] = {10, 30, 40, 90, 80, 50, 70} // 80 and 40 Swapped
+j = 5 : Since arr[j] <= pivot, do i++ and swap arr[i] with arr[j] 
+i = 3 
+arr[] = {10, 30, 40, 50, 80, 90, 70} // 90 and 50 Swapped 
+
+We come out of loop because j is now equal to high-1.
+Finally we place pivot at correct position by swapping
+arr[i+1] and arr[high] (or pivot) 
+arr[] = {10, 30, 40, 50, 70, 90, 80} // 80 and 70 Swapped 
+
+Now 70 is at its correct place. All elements smaller than
+70 are before it and all elements greater than 70 are after
+it.
+'''
