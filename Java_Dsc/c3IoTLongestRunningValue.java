49d2713b7f4eb646e332688ab2f69a8f923ed63c
Rahul Salecha <rahulgsalecha@gmail.com>
2018-03-19 20:52:56 -0700

    Improved version of longest running value

--- Java_Dsc/c3IoTLongestRunningValue.java
+++ Java_Dsc/c3IoTLongestRunningValue.java
@@ -19,6 +19,38 @@ import java.util.*;
 
 class c3IoTLongestRunningValue{
 
+public static int[] longestRunningValueImproved(int[] input){
+
+    if(input == null || input.length == 0){
+        throw new IllegalArgumentException("The input array is incorrect\n");
+    }
+    int n = input.length;
+    int[] res = new int[n];
+
+    Stack<Integer> stack = new Stack<Integer>();
+
+    if(n ==0){
+        return res;
+    }
+
+    for(int i = 0;i<n;i++){
+
+        if(stack.size()==0 || input[stack.peek()]> input[i]){
+            stack.push(i);
+            res[i]=1;
+        }
+        else{
+            while(stack.size()!=0 && input[stack.peek()]<=input[i]){
+                stack.pop();
+            }
+            res[i] = (stack.size()==0)?i+1: i-stack.peek()+1;
+            stack.push(i);
+        }
+    }
+
+    return res;
+}
+
 public static int[] longestRunningValue(int[] input, int length) {
     int[] output = new int[length];
     output[0] = 1;
@@ -67,8 +99,15 @@ public static int getPreviousLowerCounts(int[] input, int current, int index) {
 public static void main(String[] args){
     int[] input = {30,50,60,20,10,40,60,70,80};
     int[] output = longestRunningValue(input, input.length);
+    int[] output1 = longestRunningValueImproved(input);
 
     System.out.println("Input: " + Arrays.toString(input));
-    System.out.println("Output: " + Arrays.toString(output));
+    System.out.println("Output:Non-Efficient O(n^2): " + Arrays.toString(output));
+    System.out.println("Output Improved O(n): " + Arrays.toString(output1));
 }
 }
+/* Output:
+ * Input: [30, 50, 60, 20, 10, 40, 60, 70, 80]
+ * Output:Non-Efficient O(n^2): [1, 2, 3, 1, 1, 3, 7, 8, 9]
+ * Output Improved O(n): [1, 2, 3, 1, 1, 4, 7, 8, 9]
+ * */
